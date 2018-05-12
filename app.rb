329b44c4f26fb074b9c28d54e5c3cd488808fd2a
snkoski <shawnnkoski@gmail.com>
2018-05-12 19:52:44 -0400

    create routes for get requests using Sinatra and testing with Shotgun

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Shawn"
+  end
+
+  get '/hometown' do
+    "My hometown is Fort Collins, CO"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is an impossible question to answer"
+  end
+
 end
